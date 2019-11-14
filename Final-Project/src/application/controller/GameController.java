de76ca2406626a5ec7e95d2613941ffbd9b6baa3
khorsman <55725028+khorsman@users.noreply.github.com>
2019-11-14 17:28:24 -0600

    Update GameController.java

--- Final-Project/src/application/controller/GameController.java
+++ Final-Project/src/application/controller/GameController.java
@@ -1,5 +1,39 @@
 package application.controller;
 
-public class GameController {
+public class GameController implements Initializable {
 
+  // WIP
+  
+  public class Controller {
+    public Controller(Scene scene, Snake snake){
+      scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+
+			  @Override
+			  public void handle(KeyEvent k) {
+				  switch(k.getCode().toString()){
+            case "UP":
+              snake.setDirection(1);
+              break;
+            case "DOWN":
+              snake.setDirection(3);
+              break;
+            case "LEFT":
+              snake.setDirection(4);
+              break;
+            case "RIGHT":
+              snake.setDirection(2);
+              break;
+          }
+			  }
+			
+		});
+  }
+  
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+		Snake s = new Snake();
+    Controller c = new Controller(scene, s);  // get scene somehow
+    
+	}
+  
 }
