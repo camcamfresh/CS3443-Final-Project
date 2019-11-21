8769dde2b3c1be2d5eb53eb29f3a3fbe156b7d79
khorsman <55725028+khorsman@users.noreply.github.com>
2019-11-21 13:17:43 -0600

    Update GameEnder.java

--- Final-Project/src/application/model/GameEnder.java
+++ Final-Project/src/application/model/GameEnder.java
@@ -1,24 +1,48 @@
-// @author Kyle Horsman
+package application.model;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * @author Kyle Horsman
+ *
+ */
 public class GameEnder {
-  private Snake snake;
+
+	private Stage stage;
+	private Snake snake;
 	private String name;
 	
-	public GameEnder(Snake snake, String name){
+	// Constuctor
+	public GameEnder(Stage stage, Snake snake, String name){
+		this.stage = stage;
 		this.snake = snake;
+		this.name = name;
 	}
 	
+	/**
+	 * Ends game and loads ScoreController
+	 */
 	public void endGame(){
 		HighScore score = new HighScore(snake.getLength(), name);
-		try{
-			
+		//score.saveToFile();
+		
+		//testing
+		System.out.println(snake.getLength() + " " + name);
+		//
+		
+		try {
+			Pane root = FXMLLoader.load(getClass().getResource("/application/view/Score.fxml"));
+			stage.setScene(new Scene(root));
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-  	
-  	public String getName(){
-  		return name;
-  	}
-  	
-  	public Snake getSnake(){
-  		return snake;
-  	}
+	
 }
